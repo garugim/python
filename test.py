841fef1599b163c2d5d45955ea06167a0438e623
garugim <garugim@gmail.com>
2025-03-24 03:24:26 +0000

    자료형 및 연산자를 활용

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 print("Hello, World!")
 print("안녕 파이썬")
+# 구구단 프로그램 (숫자 입력 검사 추가)
+
+while True:
+    num = input("출력할 구구단을 입력하세요: ")
+
+    if num.isdigit():  # 입력값이 숫자인지 확인
+        num = int(num)
+        print(f"{num}단 출력:")
+        for i in range(1, 10):
+            print(f"{num} × {i} = {num * i}")
+        break  # 정상적인 입력이면 반복문 종료
+    else:
+        print("유효한 숫자를 입력해주세요.")  # 숫자가 아니면 경고 메시지 출력
---
+++ test1.py
@@ -0,0 +1,7 @@
+# num = int(input("출력할 구구단을 입력하세요: "))
+number = input("출력할 구구단을 입력하세요: ")
+print(f"{number}단 출력:")
+# print(f"{num}단:" f"{num}단:" f"{num}단:")
+number = int(number)
+result = number * 2
+print(result)
